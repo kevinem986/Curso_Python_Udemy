2c4b29afcdcc1a062f3448691eee7ad0f38d367d
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-05-01 01:26:36 +0200

    feat: adding valores por input

---
+++ InitPython/seccion3/ingresovalores.py
@@ -0,0 +1,13 @@
+# nombrevariable = input("mensaje")
+
+nombre = input('Ingrese el nombre: ')
+edad = int(input('Ingrese la edad: '))
+estatura = float(input('Ingrese la estatura: '))
+
+# print(nombre)
+# print(edad)
+# print(estatura)
+
+print('Hola mi nombre es {}, tengo {} y mido {} metros.'.format(nombre, edad, estatura))
+
+# Ctrl + Alt + B -> Ejecutar
