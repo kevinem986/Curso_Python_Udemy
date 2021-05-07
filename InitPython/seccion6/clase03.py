7c431e7195bb70ed2b572744af040ff1756dee67
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-05-07 04:41:13 +0200

    feat: adding instancias de clases y constructor

---
+++ InitPython/seccion6/clase03.py
@@ -0,0 +1,41 @@
+class Computador():
+
+	def __init__(self):
+		self.marca = "Toshiba"
+		self.procesador = "Intel"
+		self.mouse = 1
+		self.teclado = 1
+
+		self.suspendido = False
+
+	def ejecucion(self, suspension):
+		self.suspendido = suspension
+
+		if(self.suspendido):
+			print("El equipo está suspendido")
+		else:
+			print("El equipo está en ejecución")
+
+	def estado(self):
+		print(self.marca,self.procesador,self.mouse,self.teclado)
+		
+pc = Computador()
+
+print("Marca: ", pc.marca)
+print("Procesador: ", pc.procesador)
+print("Mouse: ", pc.mouse)
+print("Teclado: ", pc.teclado)
+
+pc.ejecucion(True)
+pc.estado()
+
+print("************************************************")
+pc2 = Computador()
+
+print("Marca: ", pc2.marca)
+print("Procesador: ", pc2.procesador)
+print("Mouse: ", pc2.mouse)
+print("Teclado: ", pc2.teclado)
+
+pc2.ejecucion(False)
+pc2.estado()
