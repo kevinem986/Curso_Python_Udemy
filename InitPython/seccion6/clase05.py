9c3c877de6bd7f1d85a6d021d9728b2fc10836f4
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-05-11 22:35:54 +0200

    feat: adding herencia

---
+++ InitPython/seccion6/clase05.py
@@ -0,0 +1,54 @@
+class Persona():
+
+	def __init__(self, nombre, apellido, sexo):
+		self.nombre = nombre
+		self.apellido = apellido
+		self.sexo = sexo
+
+		self.hablar = False
+
+	def hablar(self):
+		self.hablar = True
+
+	def estado(self):
+		print("Nombre: ", self.nombre,
+			  "\nApellido: ", self.apellido,
+			  "\nSexo: ", self.sexo,
+			  "\nHablar: ", self.hablar)
+
+class Supervisor(Persona):
+	objetivo = ""
+
+	def objetivos(self):
+		self.objetivo = "Tiene que cumplir con las metas mensuales."
+
+	def estado(self):
+		print("Nombre: ", self.nombre,
+			  "\nApellido: ", self.apellido,
+			  "\nSexo: ", self.sexo,
+			  "\nHablar: ", self.hablar,
+			  "\nObjetivo: ", self.objetivo)
+
+class Obrero(Persona):
+	manejarMaq = ""
+
+	def manejar(self):
+		self.manejarMaq = "Descargar 80 containers al mes."
+
+	def estado(self):
+		print("Nombre: ", self.nombre,
+			  "\nApellido: ", self.apellido,
+			  "\nSexo: ", self.sexo,
+			  "\nHablar: ", self.hablar,
+			  "\nObjetivo: ", self.manejarMaq)
+
+print("*******************DATOS SUPERVISOR*****************************")
+miSupervisor = Supervisor("Juan", "Gonzalez", "Masculino")
+miSupervisor.objetivos()
+miSupervisor.estado()
+
+print("********************DATOS OBRERO****************************")
+miObrero = Obrero("Jose", "Amaranto", "Masculino")
+miObrero.manejar()
+miObrero.estado()
+		
