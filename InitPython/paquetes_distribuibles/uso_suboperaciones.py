0c66c3b278cc64d65ac39d4c8ca8ba9f5aa9271f
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-05-12 03:39:30 +0200

    feat: adding paquetes redistribuibles, sort foldes and files project

---
+++ InitPython/paquetes_distribuibles/uso_suboperaciones.py
@@ -0,0 +1,13 @@
+# from nombre_paquete.nombre_modulo import nombre_funcion
+# from paqoperaciones.operaciones import *
+
+# from paquetes.paqoperaciones.opbasicas.basicas import multiplicacion
+from paquetes.paqoperaciones.opbasicas.basicas import *
+
+resta(34,76)
+suma(12,8)
+multiplicacion(9,19)
+division(90,5)
+
+# from paqoperaciones.opotros.otros import redondeo
+# redondeo(9.4)
---
+++ InitPython/seccion7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name = "packs",
+	version = "1.0",
+	description = "Operaciones Matemáticas Básicas",
+	author = "Kevinem986",
+	url = "https://www.linkedin.com/in/kevinem986/",
+	packages = ["paquetes","paquetes.paqoperaciones.opbasicas"]
+)
